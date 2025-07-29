ec2d04c287c5b9d256fbd14ce0cd4158976ab516
xKaLiOx <linas159159123@gmail.com>
2025-07-29 21:05:29 +0300

    Fixup voltage reading and overcurrent protection

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -113,39 +113,39 @@ uint16_t CurrentToVoltage(uint32_t Shunt_voltage);//voltage on sense resistor
 /* USER CODE END 0 */
 
 /**
- * @brief  The application entry point.
- * @retval int
- */
+  * @brief  The application entry point.
+  * @retval int
+  */
 int main(void)
 {
 
-	/* USER CODE BEGIN 1 */
+  /* USER CODE BEGIN 1 */
 	static uint32_t last_tick;
-	/* USER CODE END 1 */
+  /* USER CODE END 1 */
 
-	/* MCU Configuration--------------------------------------------------------*/
+  /* MCU Configuration--------------------------------------------------------*/
 
-	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-	HAL_Init();
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-	/* USER CODE BEGIN Init */
-	/* USER CODE END Init */
+  /* USER CODE BEGIN Init */
+  /* USER CODE END Init */
 
-	/* Configure the system clock */
-	SystemClock_Config();
+  /* Configure the system clock */
+  SystemClock_Config();
 
-	/* USER CODE BEGIN SysInit */
+  /* USER CODE BEGIN SysInit */
 
-	/* USER CODE END SysInit */
+  /* USER CODE END SysInit */
 
-	/* Initialize all configured peripherals */
-	MX_GPIO_Init();
-	MX_DMA_Init();
-	MX_TIM4_Init();
-	MX_TIM3_Init();
-	MX_ADC1_Init();
-	MX_TIM2_Init();
-	/* USER CODE BEGIN 2 */
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_TIM4_Init();
+  MX_TIM3_Init();
+  MX_ADC1_Init();
+  MX_TIM2_Init();
+  /* USER CODE BEGIN 2 */
 	HAL_GPIO_WritePin(DISCHARGE_STOP_GPIO_Port, DISCHARGE_STOP_Pin,GPIO_PIN_SET);// OP AMP inv input to turn off discharge
 
 
@@ -172,10 +172,10 @@ int main(void)
 	 * @brief Init in 4 bit LCD 16x2
 	 */
 	LCD_Init();
-	/* USER CODE END 2 */
+  /* USER CODE END 2 */
 
-	/* Infinite loop */
-	/* USER CODE BEGIN WHILE */
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
 	while (1)
 	{
 		if(updateScreenRequest)//only perform LCD switch states on gpio change
@@ -275,10 +275,10 @@ int main(void)
 					HAL_GPIO_WritePin(USER_LED_GPIO_Port, USER_LED_Pin, GPIO_PIN_SET);
 					Error_Handler();
 				}
-				//STATE_MCU_PREVIOUS = STATE_MCU_CURRENT; IMPLEMENTED IN BUTTON
+				STATE_MCU_PREVIOUS = STATE_MCU_CURRENT; //IMPLEMENTED IN BUTTON THE START
 				HAL_GPIO_WritePin(CHARGING_STATE_GPIO_Port, CHARGING_STATE_Pin,GPIO_PIN_SET); // indication for charging
 				HAL_GPIO_WritePin(DISCHARGE_STOP_GPIO_Port, DISCHARGE_STOP_Pin,GPIO_PIN_RESET);
-				last_tick = uwTick;
+				updateScreenRequest = 1;
 				break;
 			}
 			break;
@@ -394,52 +394,52 @@ int main(void)
 		}
 	}
 
-	/* USER CODE END WHILE */
+    /* USER CODE END WHILE */
 
-	/* USER CODE BEGIN 3 */
-	/* USER CODE END 3 */
+    /* USER CODE BEGIN 3 */
+  /* USER CODE END 3 */
 }
 
 /**
- * @brief System Clock Configuration
- * @retval None
- */
+  * @brief System Clock Configuration
+  * @retval None
+  */
 void SystemClock_Config(void)
 {
-	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-	RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-	/** Initializes the RCC Oscillators according to the specified parameters
-	 * in the RCC_OscInitTypeDef structure.
-	 */
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-	{
-		Error_Handler();
-	}
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-	/** Initializes the CPU, AHB and APB buses clocks
-	 */
-	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-			|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSE;
-	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSE;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-	{
-		Error_Handler();
-	}
-	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC;
-	PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV2;
-	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-	{
-		Error_Handler();
-	}
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC;
+  PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV2;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
 /* USER CODE BEGIN 4 */
@@ -547,12 +547,11 @@ void updateScreen()
 	case  FINISH:
 	{
 		sprintf(LCD_buffer,"DISCHARGED");
-		formatCharToLCD(LCD_buffer,0,1,ALIGN_CENTER);
-
+		formatCharToLCD(LCD_buffer,0,0,ALIGN_CENTER);
 
 		uint32_t elapsed_time = (uwTick-DischargeDisplayData.start_time)/1000;
 		sprintf(LCD_buffer,"%u mAh in %lu s?",(uint16_t)DischargeDisplayData.capacity_mah,elapsed_time);
-		formatCharToLCD(LCD_buffer,0,1,ALIGN_RIGHT);
+		formatCharToLCD(LCD_buffer,0,1,ALIGN_CENTER);
 
 		break;
 	}
@@ -645,7 +644,7 @@ void PWM_Control_loop(TIM_HandleTypeDef *htim, uint16_t *current, uint16_t volta
 		OverCurrent_indication = 1;
 	}
 
-	if(Cutoff_voltage < (voltage*1000))
+	if(Cutoff_voltage > voltage)
 	{
 		HAL_GPIO_WritePin(DISCHARGE_STOP_GPIO_Port, DISCHARGE_STOP_Pin,GPIO_PIN_SET);
 		STATE_MCU_CURRENT = FINISH;
@@ -689,7 +688,7 @@ void HAL_ADC_ConvHalfCpltCallback(ADC_HandleTypeDef* hadc)
 
 	//CURRENT AND MAH CONVERSION
 	Current_BATT = 3300*adc_sum[1]/R_load/ADC_steps;//convert to mA
-	Voltage = (uint16_t)((Vdda*adc_sum[0])*1.0/ADC_steps*R_divider_multiplier);//in volts
+	Voltage = (uint16_t)((Vdda*adc_sum[0])*1000/ADC_steps*R_divider_multiplier);//in mV
 	PWM_Control_loop(&htim2,&Current_BATT,Voltage);
 	DischargeDisplayData.capacity_mah += Current_BATT*MAH_CONVERSION;
 
@@ -720,7 +719,7 @@ void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
 
 	//CURRENT AND MAH CONVERSION
 	Current_BATT = 3300*adc_sum[1]/R_load/ADC_steps;//convert to mA
-	Voltage = (uint16_t)Vdda*adc_sum[0]/ADC_steps*R_divider_multiplier;//in volts
+	Voltage = (uint16_t)((Vdda*adc_sum[0])*1000/ADC_steps*R_divider_multiplier);//in mV
 	PWM_Control_loop(&htim2,&Current_BATT,Voltage);
 	DischargeDisplayData.capacity_mah += Current_BATT*MAH_CONVERSION;
 
@@ -745,15 +744,16 @@ int _write(int le, char *ptr, int len)
 	return len;
 }
 
+
 /* USER CODE END 4 */
 
 /**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
 void Error_Handler(void)
 {
-	/* USER CODE BEGIN Error_Handler_Debug */
+  /* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	__disable_irq();
 	while (1)
@@ -761,21 +761,21 @@ void Error_Handler(void)
 		HAL_GPIO_TogglePin(USER_LED_GPIO_Port, USER_LED_Pin);
 		HAL_Delay(200);
 	}
-	/* USER CODE END Error_Handler_Debug */
+  /* USER CODE END Error_Handler_Debug */
 }
 #ifdef USE_FULL_ASSERT
 /**
- * @brief  Reports the name of the source file and the source line number
- *         where the assert_param error has occurred.
- * @param  file: pointer to the source file name
- * @param  line: assert_param error line source number
- * @retval None
- */
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-	/* USER CODE BEGIN 6 */
+  /* USER CODE BEGIN 6 */
 	/* User can add his own implementation to report the file name and line number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-	/* USER CODE END 6 */
+  /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
--- Debug/Core/Startup/subdir.mk
+++ Debug/Core/Startup/subdir.mk
@@ -16,7 +16,7 @@ S_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Core/Startup/%.o: ../Core/Startup/%.s Core/Startup/subdir.mk
-	arm-none-eabi-gcc -mcpu=cortex-m3 -g3 -DDEBUG -c -I"C:/Users/Linas/Desktop/STM32_stuff/Projects_older_workspace/Battery_analyzer/libraries/HD44780_LCD" -I"C:/Users/Linas/Desktop/STM32_stuff/Projects_older_workspace/Battery_analyzer/libraries" -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@" "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m3 -g3 -DDEBUG -c -I"C:/Users/Linas/Desktop/STM32_stuff/Projects_older_workspace/Battery_analyzer/libraries/HD44780_LCD" -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@" "$<"
 
 clean: clean-Core-2f-Startup
 
